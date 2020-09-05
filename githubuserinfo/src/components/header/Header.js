22877345c5a395f32e86e591a68d6d918f53fba2
Darren Tebo <darrentebo83@outlook.com>
2020-09-05 16:56:24 -0400

    created file structure and header component

---
+++ githubuserinfo/src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = props => {
+    return(
+        <div className='header'>
+            <section className='logo'>
+
+            </section>
+            <section className='nav'>
+
+            </section>
+        </div>
+    )
+}
