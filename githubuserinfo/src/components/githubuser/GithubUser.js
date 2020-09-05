d1d2076e34c7356d44186a781c11026ba4693749
Darren Tebo <darrentebo83@outlook.com>
2020-09-05 17:12:43 -0400

    made axios request for user data and set that data into state

--- githubuserinfo/src/components/githubuser/GithubUser.js
+++ githubuserinfo/src/components/githubuser/GithubUser.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class GithubUser extends Component{
     state = {
         userinfo: {}
     };
+
+    componentDidMount(){
+        this.getUser('dtebo');
+    }
+
+    getUser = username => {
+        const BASE_URL = 'https://api.github.com/users';
+
+        axios.get(`${BASE_URL}/${username}`)
+            .then(resp => {
+                this.setState({
+                    userinfo: resp.data
+                });
+
+                console.log('dt: Github: getUser: ', this.state.userinfo);
+            })
+            .catch(err => {
+                console.log('dt: GithubUser: getUser: error: ', err.message);
+            });
+    }
 
     render(){
         return(
